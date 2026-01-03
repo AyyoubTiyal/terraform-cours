cb74fc7f6b9d8d4c6dc69ccc0f6006ed48d99ece
bouyya <tiyalayyoub@gmail.com>
2026-01-03 02:10:36 +0100

    update file

--- functions/function.tf
+++ functions/function.tf
@@ -6,5 +6,6 @@ resource "aws_iam_user_policy" "lb_ro" {
     name = "demouser_policy"
     user = aws_iam_user.this.name
 
+    #attach policy from external file 
     policy = file("policy.json")
 }
