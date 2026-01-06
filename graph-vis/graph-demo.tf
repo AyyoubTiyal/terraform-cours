58e7ca8b7395d698eb341a88aaa794a6f8e32239
bouyya <tiyalayyoub@gmail.com>
2026-01-06 20:38:52 +0100

    vizualise grphe with terraform plan

---
+++ graph-vis/graph-demo.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_user" "user" { 
+    name = "user.${count.index}"
+    count=3
+    path = "/system/"
+}
+
+resource "aws_iam_user_policy" "user_policy" {
+  count = 3
+  user  = aws_iam_user.user[count.index].name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "s3:ListBucket"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_instance" "ec2" {
+    ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID
+    instance_type = "t2.micro"
+    count         = 3
+    
+    tags = {
+        Name = "ec2-instance-${count.index}"
+    }
+}
+
+
+#In the terminal to visualize the resource graph, run:
+# terraform graph -type=plan | dot -Tsvg > graph.svg
