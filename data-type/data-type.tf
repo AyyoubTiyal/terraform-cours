dcc78d71af06f3011fc8e7068712b28b6c48c099
bouyya <tiyalayyoub@gmail.com>
2026-01-07 22:07:01 +0100

    for eachY

--- data-type/data-type.tf
+++ data-type/data-type.tf
@@ -3,6 +3,26 @@ variable "username" {
    type = number
 }
 
-resource "aws_iam_user" "lb" {
-    name = var.username
+variable "tags-set" {
+    # the order is not preserved and duplicates are removed
+    type = set(string)
+    default = ["dev", "test", "dev"]
 }
+
+variable "tags-list" {
+    # the order is preserved and duplicates are allowed
+    type = list(string)
+    default = ["dev", "test", "dev"]
+}
+
+output "tags-set" {
+    value = var.tags-set
+}
+
+output "tags-list" {
+   value = var.tags-list
+}
+
+/*resource "aws_iam_user" "lb" {
+    name = var.username
+}*/
--- meta-argument/for-each.tf
+++ meta-argument/for-each.tf
@@ -1,5 +1,4 @@
 variable "iam_names" {
-
   type    = set(string)
   default = ["admin", "developer", "tester", "manager"]
 }
@@ -8,4 +7,22 @@ resource "aws_iam_user" "users" {
     for_each = var.iam_names
     name  = each.value
     path  = "/"
+}
+
+variable "mymap" {
+  type = map
+  default = {
+    dev = "ami-123"
+    prod = "ami-567"
+  }
+}
+
+resource "aws_instance" "web" {
+  for_each = var.mymap
+  ami = each.key
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = each.value
+  }
 }
