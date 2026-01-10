2c66e7cbbc283040af63f9f3d917ad3dadd02676
bouyya <tiyalayyoub@gmail.com>
2026-01-10 23:15:11 +0100

    outputs in modules

--- modules/modules/ec2/main.tf
+++ modules/modules/ec2/main.tf
@@ -21,3 +21,7 @@ resource "aws_instance" "ec2_instance" {
     }
   
 }
+
+output "instance_id" {
+  value = aws_instance.ec2_instance.id
+}
--- modules/teams/A/create-ec2.tf
+++ modules/teams/A/create-ec2.tf
@@ -1,6 +1,15 @@
 module "myec2"  {
   source = "../../modules/ec2"
-  ami = "ami-0c55b159cbfafe1f0"
+  ami = "ami-078abd88811000d7e"
   instance_type = "t3.micro"
   region = "eu-west-3"  
+}
+
+
+resource "aws_eip" "lb" {
+# Associate the EIP with the EC2 instance created by the module
+# Use module output to get the instance ID
+# the output is declared in modules/ec2/main.tf
+  instance = module.myec2.instance_id
+  domain = "vpc"
 }
